6423e1315483f562c2000e5c5b8a82d8f9a99484
Jimmy Le <jimmyle414@gmail.com>
2020-02-28 20:15:25 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,6 @@
 # code your #position_taken? method here!
 
+board = ["X", "X","nil", " "," ", " "," ", " ","O"]
 def position_taken(board, index)
-  board = [" ", " "," ", " "," ", " "," ", " "," "]
+  
 end
