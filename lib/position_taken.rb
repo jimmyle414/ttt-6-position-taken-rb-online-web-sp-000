eadef2ef26c37038e081939e0ea684ab2f836e59
Jimmy Le <jimmyle414@gmail.com>
2020-02-28 20:18:57 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 
-board = ["X", "X","nil", " "," ", " "," ", " ","O"]
+board = ["",""," "," ","X"," "," "," ",""]
 def position_taken(board, index)
   index = board[location.to_i-1]
   index != " " && index != "" && index != nil
